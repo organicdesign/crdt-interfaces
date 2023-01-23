bd87e517d3ec40824d388c1ab25b149f118e5759
saul <saul@organicdesign.co.nz>
2023-01-24 12:38:29 +1300

    Add broadcaster types.

--- src/crdt.d.ts
+++ src/crdt.d.ts
@@ -1,5 +1,20 @@
-export type BroadcastHandler = (data: Uint8Array) => void;
+// Broadcast
+export interface BroadcastData {
+	protocol: string
+	data: Uint8Array
+}
 
+export type BroadcastHandler = (data: BroadcastData) => void;
+
+export interface CRDTBroadcaster {
+	protocol: string
+	addBroadcaster (broadcaster: BroadcastHandler): void
+	onBroadcast: BroadcastHandler
+}
+
+export type CreateBroadcaster<T extends CRDTBroadcaster=CRDTBroadcaster> = () => T
+
+// Sync
 export interface SyncContext {
 	id: Uint8Array
 	syncId: number
@@ -10,13 +25,19 @@ export interface CRDTSynchronizer {
 	sync (data: Uint8Array | undefined, context: SyncContext): Uint8Array | undefined
 }
 
+export type CreateSynchronizer<T extends CRDTSynchronizer=CRDTSynchronizer> = (components?: Record<string, any>) => T
+
+// CRDT
 export interface CRDT {
 	id: Uint8Array
+	toValue (): unknown
+
 	getProtocols (): Iterable<string>
 	getSynchronizer (protocol: string): CRDTSynchronizer | undefined
-	toValue (): unknown
+
 	serialize? (): Uint8Array
 	deserialize? (data: Uint8Array): void
+
 	addBroadcaster? (broadcaster: BroadcastHandler): void
 	onBroadcast?: BroadcastHandler
 }
@@ -24,7 +45,8 @@ export interface CRDT {
 export interface CRDTConfig {
 	id: Uint8Array
 	generateTimestamp?: () => number
+	synchronizers: CreateSynchronizer[]
+	broadcasters: CreateBroadcaster[]
 }
 
 export type CreateCRDT<T extends CRDT=CRDT> = (config: CRDTConfig) => T;
-export type CreateSynchronizer<T extends CRDTSynchronizer=CRDTSynchronizer> = (options?: Record<string, any>)  => T
